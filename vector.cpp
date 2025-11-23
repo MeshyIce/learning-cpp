01c7f45dc676d02fd1ddc832b4a680be28e22cac
Yuval Meshorer <yuval@mind.io>
2025-11-23 13:29:32 +0200

    format code

--- vector.cpp
+++ vector.cpp
@@ -3,80 +3,98 @@
 #include <iostream>
 #include <initializer_list>
 
-class VectorException : public std::exception {
+class VectorException : public std::exception
+{
 private:
     std::string errorMessage;
 
 public:
-    VectorException(const std::string& message) {
+    VectorException(const std::string &message)
+    {
         this->errorMessage = message;
     }
 
-    const char* what() const override {
+    const char *what() const noexcept override
+    {
         return this->errorMessage.c_str();
     }
 };
 
-class VectorIndexOutOfRange : public VectorException {
+class VectorIndexOutOfRange : public VectorException
+{
 public:
     VectorIndexOutOfRange(unsigned int index)
         : VectorException("Index " + std::to_string(index) + " is out of range")
-    {}
+    {
+    }
 };
 
-class VectorIsEmpty : public VectorException {
+class VectorIsEmpty : public VectorException
+{
 public:
     VectorIsEmpty() : VectorException("Vector is empty!") {}
 };
 
 template <typename T>
-class Vector {
+class Vector
+{
 private:
-    T* array;
+    T *array;
     unsigned int arraySize;
     unsigned int arrayCapacity;
 
 public:
-    Vector() {
+    Vector()
+    {
         this->array = new T[0];
         this->ararySize = 0;
         this->arrayCapacity = 0;
     }
 
-    Vector(std::initializer_list<T> input) {
+    Vector(std::initializer_list<T> input)
+    {
         // this->arraySize, this->arrayCapacity are set in this->add
-        for (T value : input) {
+        for (T value : input)
+        {
             this->add(value);
         }
     }
 
-    void erase(unsigned int index) {
-        if (this->arraySize == 0) {
+    void erase(unsigned int index)
+    {
+        if (this->arraySize == 0)
+        {
             throw VectorIsEmpty();
         }
-        
-        if (index >= this->arraySize) {
+
+        if (index >= this->arraySize)
+        {
             throw VectorIndexOutOfRange(index);
         }
 
-        for (unsigned int i = index; i < this->arraySize - 1; i++) {
+        for (unsigned int i = index; i < this->arraySize - 1; i++)
+        {
             this->array[i] = this->array[i + 1];
         }
 
         this->arraySize--;
     }
 
-    void reserve(unsigned int newCapacity) {
-        if (this->arrayCapacity >= newCapacity) {
+    void reserve(unsigned int newCapacity)
+    {
+        if (this->arrayCapacity >= newCapacity)
+        {
             return;
         }
 
-        T* newArray = new T[newCapacity];
+        T *newArray = new T[newCapacity];
 
-        for (unsigned int i = 0; i < this->arraySize; i++) {
+        for (unsigned int i = 0; i < this->arraySize; i++)
+        {
             newArray[i] = this->array[i];
         }
-        for (unsigned int i = this->arraySize; i < newCapacity; i++) {
+        for (unsigned int i = this->arraySize; i < newCapacity; i++)
+        {
             newArray[i] = T();
         }
 
@@ -85,20 +103,25 @@ public:
         this->arrayCapacity = newCapacity;
     }
 
-    void clear() {
+    void clear()
+    {
         delete[] this->array;
         this->array = nullptr;
         this->arraySize = 0;
         this->arrayCapacity = 0;
     }
 
-    void resize(unsigned int newSize, T newValue = T()) {
-        if (newSize > this->arrayCapacity) {
+    void resize(unsigned int newSize, T newValue = T())
+    {
+        if (newSize > this->arrayCapacity)
+        {
             this->reserve(newSize);
         }
 
-        if (newSize > this->arraySize) {
-            for (unsigned int i = this->arraySize; i < newSize; i++) {
+        if (newSize > this->arraySize)
+        {
+            for (unsigned int i = this->arraySize; i < newSize; i++)
+            {
                 this->array[i] = newValue;
             }
         }
@@ -106,8 +129,10 @@ public:
         this->arraySize = newSize;
     }
 
-    void add(T value) {
-        if (this->arraySize >= this->arrayCapacity) {
+    void add(T value)
+    {
+        if (this->arraySize >= this->arrayCapacity)
+        {
             this->reserve(this->arraySize + 1);
         }
 
@@ -115,21 +140,25 @@ public:
         this->arraySize++;
     }
 
-    void print() {
-        for (unsigned int i = 0; i < this->arraySize; i++) {
+    void print()
+    {
+        for (unsigned int i = 0; i < this->arraySize; i++)
+        {
             std::cout << this->array[i] << ", ";
         }
         std::cout << "\n";
     }
 
-    int size() {
+    int size()
+    {
         return this->arraySize;
     }
 };
 
-int main()
+void testBasicVector()
 {
-    Vector<int> v{ 1, 2, 3, 4, 5 };
+    Vector<int> v{1, 2, 3, 4, 5};
+
     std::cout << "Initial vector: ";
     v.print();
 
@@ -146,10 +175,12 @@ int main()
     v.print();
     std::cout << "Size: " << v.size() << "\n";
 
-    try {
+    try
+    {
         v.erase(1000);
     }
-    catch (const VectorIndexOutOfRange& e) {
+    catch (const VectorIndexOutOfRange &e)
+    {
         std::cout << "Successfully received exception: " << e.what() << std::endl;
     }
 
@@ -163,10 +194,17 @@ int main()
     v.print();
     std::cout << "Size: " << v.size() << "\n";
 
-    try {
+    try
+    {
         v.erase(0);
     }
-    catch (const VectorIsEmpty& e) {
+    catch (const VectorIsEmpty &e)
+    {
         std::cout << "Successfully received exception: " << e.what() << std::endl;
     }
+}
+
+int main()
+{
+    testBasicVector();
 }
